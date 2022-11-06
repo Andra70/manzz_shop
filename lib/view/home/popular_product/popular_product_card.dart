c2a755fbffdb8a02f75eb4bc2d7dc8b2cc5b10d4
Andra <abhinayaandra@gmail.com>
2022-11-06 23:26:54 +0700

    Feat: [IB] Bottom Navigation Bar

--- lib/view/home/popular_product/popular_product_card.dart
+++ lib/view/home/popular_product/popular_product_card.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import '/const.dart';
+import 'package:manzz_shop/const.dart';
 import 'package:shimmer/shimmer.dart';
 import '../../../../model/product.dart';
 
